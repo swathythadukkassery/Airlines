c07a9856be1396f83c7ba4383bdd84de686488ee
Swathy <swathypanayoor@gmail.com>
2021-01-27 19:30:22 +0530

    first commit

---
+++ airlines.py
@@ -0,0 +1,20 @@
+import json
+
+with open('airlines.csv', 'r') as f:
+    d = {}
+    count, val = 0, 0
+    for line in f:
+        each = line.split(',')
+        if each[1] == "Airport.Name":
+            continue
+        each = each[1] + "," + each[2]
+        if each in d:
+            d[each]+=1
+        else:
+            d[each] = 1
+    Keymax = max(d, key=d.get) 
+    Keymin = min(d, key=d.get)
+    final_d = {Keymax.replace('"',''):d[Keymax], Keymin.replace('"',''):d[Keymin]}
+    print (final_d)
+    final = json.dumps(final_d)
+    print(final)
